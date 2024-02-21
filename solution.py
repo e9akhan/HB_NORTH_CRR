ece7980ecb5f8aa0c0420567407259c1dfeebaf5
Akbar Khan <akbar.khan@enine.school>
2024-02-21 14:58:30 +0530

    Commit

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@
     Module name :- solution
 """
 
+
 import os
 import csv
 from datetime import datetime
@@ -9,11 +10,11 @@ from datetime import datetime
 
 def load_csv(filepath):
     """
-    Load data.
+        Load data.
     """
     filepath = os.path.abspath(filepath)
 
-    with open(filepath, "r", encoding="utf-8-sig") as f:
+    with open(filepath, 'r', encoding='utf-8-sig') as f:
         reader = list(csv.DictReader(f))
 
     return reader
@@ -21,19 +22,19 @@ def load_csv(filepath):
 
 def filter_data_by_date(data):
     """
-    Filter data by date.
+        Filter data by date.
     """
-    return sorted(data, key=lambda x: x.get("fordate"))
+    return sorted(data, key = lambda x: x.get('fordate'))
 
 
 def month_data(data, month, idx):
     """
-    Month data.
+        Month data.
     """
     month_list = []
     while idx < len(data):
         record = data[idx]
-        date_obj = datetime.strptime(record["fordate"], "%Y-%m-%d")
+        date_obj = datetime.strptime(record['fordate'], '%Y-%m-%d')
 
         if date_obj.month != month:
             break
@@ -46,14 +47,14 @@ def month_data(data, month, idx):
 
 def monthly_data(data):
     """
-    Monthly Data.
+        Monthly Data.
     """
     idx = 0
     monthly_list = []
 
     while idx < len(data):
         record = data[idx]
-        month = datetime.strptime(record["fordate"], "%Y-%m-%d").month
+        month = datetime.strptime(record['fordate'], '%Y-%m-%d').month
         idx, month_list = month_data(data, month, idx)
         monthly_list.append(month_list)
 
@@ -62,15 +63,15 @@ def monthly_data(data):
 
 def filter_by_shape(data):
     """
-    Filter by shape.
+        Filter by shape.
     """
-    shapes = ["Off-peak", "PeakWD", "PeakWE"]
+    shapes = ['Off-peak', 'PeakWD', 'PeakWE']
     off_peak, peak_we, peak_wd = [], [], []
 
     for record in data:
-        if record["shape"] == shapes[0]:
+        if record['shape'] == shapes[0]:
             off_peak.append(record)
-        elif record["shape"] == shapes[1]:
+        elif record['shape'] == shapes[1]:
             peak_wd.append(record)
         else:
             peak_we.append(record)
@@ -80,31 +81,25 @@ def filter_by_shape(data):
 
 def main():
     """
-    Main.
+        Main.
     """
-    data = load_csv("HB_NORTH_CRR.csv")
+    data = load_csv('HB_NORTH_CRR.csv')
     filtered_data = filter_data_by_date(data)
     monthly_records = monthly_data(filtered_data)
     records = []
 
     for month_record in monthly_records:
         off_peak_list, peak_wd_list, peak_we_list = filter_by_shape(month_record)
-
-        off_peak = sorted(off_peak_list, key=lambda x: x.get("sequence"), reverse=True)[
-            -1
-        ]
-        peak_wd = sorted(peak_wd_list, key=lambda x: x.get("sequence"), reverse=True)[
-            -1
-        ]
-        peak_we = sorted(peak_we_list, key=lambda x: x.get("sequence"), reverse=True)[
-            -1
-        ]
+        
+        off_peak = sorted(off_peak_list, key = lambda x: x.get('sequence'), reverse=True)[-1]
+        peak_wd = sorted(peak_wd_list, key = lambda x: x.get('sequence'), reverse=True)[-1]
+        peak_we = sorted(peak_we_list, key = lambda x: x.get('sequence'), reverse=True)[-1]
 
         records += [off_peak, peak_wd, peak_we]
 
-    filepath = os.getcwd() + "/answer.csv"
+    filepath = os.getcwd() + '/answer.csv'
 
-    with open(filepath, "w", encoding="utf-8") as f:
+    with open(filepath, 'w', encoding='utf-8') as f:
         headers = records[0].keys()
         writer = csv.DictWriter(f, fieldnames=headers)
         writer.writeheader()
@@ -112,6 +107,5 @@ def main():
 
     return filepath
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(main())
